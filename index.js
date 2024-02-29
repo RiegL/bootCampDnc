7a90dcd6c30a2a6fd1fa42dbfe433693b8c652d5
LabHasar <brlabhasar1@gmail.com>
2024-02-28 21:08:25 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// // let n1 = prompt("digite um numero:")
+// // let n2 = prompt("digite um numero:")
+
+
+// // alert(mult(n1,n2));
+ 
+// // function mult(a,b){
+// //     let c = a*b
+// //     return c
+// // }
+
+
+// // const array = ["limao","morango","goiba", "laranja","kiwi"]
+ 
+ 
+// // window.alert(array)
+
+// // array.splice(1,2)
+
+// // window.alert(array)
+
+
+// // document.querySelector("#menu").style.backgroundColor = 'blue'
+// window.document.getElementById("menu").style.backgroundColor = 'blue'
